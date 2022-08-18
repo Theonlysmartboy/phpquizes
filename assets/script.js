cab09e8f0d5f082d95ce773100f76a770d118c10
Tosby <o2jose43@gmail.com>
2022-08-18 11:16:14 +0300

    begun adding instructions

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,7 @@ $(function () {
 	$("#sod").on("click", function () {
 		$("#sod").addClass("active");
         $("#en").removeClass("active");
+        $("#on").removeClass("active");
         $(".container").empty();
 		$(".container").append(
 			" <h3 class='text-success'>Sum Of Digits </h3>" +
@@ -18,11 +19,30 @@ $(function () {
     $("#en").on("click", function () {
 		$("#en").addClass("active");
         $("#sod").removeClass("active");
+        $("#on").removeClass("active");
         $(".container").empty();
 		$(".container").append(
 			" <h3 class='text-success'>Even Numbers </h3>" +
 				"<p class='text-md'>Write a PHP program to check if a number is an odd or an even number."+
                 "If it is an odd number the program should print; <strong>Odd number</strong> otherwise it should print; <strong>Even number</strong>" +
+				"<strong><br>Example 1: </strong>" +
+				"<br>Input: 23 <br>" +
+				"Output: odd number<br>" +
+				"<strong>Example 2: </strong><br>" +
+				"Input: 12<br>" +
+				"Output: even number</p>"
+		);
+	});
+
+    $("#on").on("click", function () {
+		$("#on").addClass("active");
+        $("#sod").removeClass("active");
+        $("#en").removeClass("active");
+        $(".container").empty();
+		$(".container").append(
+			" <h3 class='text-success'>Prime number</h3>" +
+				"<p class='text-md'>Write a PHP program to check if a number is a prime number."+
+                "If it is a prime number the program should print; <strong>prime number</strong> otherwise it should print; <strong>not a prime number</strong>" +
 				"<strong><br>Example 1: </strong>" +
 				"<br>Input: 23 <br>" +
 				"Output: odd number<br>" +
